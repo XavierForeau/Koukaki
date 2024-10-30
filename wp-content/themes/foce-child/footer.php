21f4073f7e831483a6b3d3093fb712781800f455
Xavier Foreau <xavier.foreau.ocr@laposte.net>
2024-10-30 18:04:50 +0100

    Commit au 30 10 24

---
+++ wp-content/themes/foce-child/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fleurs_d\'oranger_&_Chats_errants
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+        <ul>
+            <li><a href="#">My Mentions Légales</a></li>
+            <li><a href="#">MY STUDIO KOUKAKI</a></li>
+            <li><a href="#">My Contacts</a></li>
+        </ul>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
